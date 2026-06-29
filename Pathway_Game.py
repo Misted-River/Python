5b7dba94363e510810356f149a3b7c614a86a4eb
Misted-River <elli.kourousi@gmail.com>
2026-06-29 20:40:34 +0300

    update

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -1,34 +1,42 @@
 import pygame
-import sys
 
 pygame.init() # initialise pygame module
 
 position = (0,0)
-text_col = (200, 100, 0)
 clock = pygame.time.Clock()
+bottom_line_height = 10
+
+x = 1920
+y = 1080
 
 # set up objects
 player = pygame.image.load('comet_placeholder.png')# player image -> surface2
 
 fontObj = pygame.font.Font(None, 32)
-textSufaceObj = fontObj.render('blah blah', True, text_col, None) # antialiasing is true
+textSufaceObj = fontObj.render('Welcome', True, 'BLack', None) # antialiasing is true
 
 textRectObj = textSufaceObj.get_rect()
 player_pos = player.get_rect()
 
-#clock = pygame.time.Clock()
+clock = pygame.time.Clock()
 #----------------------------------------------------------------------------------
 canvas= pygame.display.set_mode((1920,1080),pygame.RESIZABLE) # canvas size -> creates screen -> background 
 image = pygame.image.load('Untitled2186_20240824160114.png').convert() # initialise image -> surface2
 
+player_pos.midbottom = (canvas.get_width() // 2, canvas.get_height() - bottom_line_height - 15)
 
 image= pygame.transform.smoothscale(image,(1920,1080)) # sizing image
+
 pygame.display.set_caption("Welcome to The Pathways") # name
 
 exit = False
 run = True
 refresh = False
 mouse = pygame.mouse.get_pos()
+
+objects = [] # gathering all players etc objects in game
+
+pygame.draw.line(canvas, 'Black', (0, canvas.get_height() - bottom_line_height), (canvas.get_width(), canvas.get_height() - bottom_line_height), 3)
 
 pygame.event.get()
 pygame.display.set_icon(image)
@@ -37,11 +45,17 @@ pygame.display.set_icon(image)
 
 class game_object:
     def __init__(self,image,height,speed):
-        pass
+        self.speed = speed
+        self.image = image
+        self.pos = image.get_rect().move(0,height)
+    def move(self):
+        self.pos = self.pos.move(self.speed,0)
+        if self.pos.right > 600:
+            self.pos.left = 0
 
 while not exit:
     #canvas.blit(image, dest = position) # render image onto surface
-    canvas.blit(image, dest="position") # render image onto surface
+    canvas.blit(image, dest=position) # render image onto surface, background
     canvas.blit(player, player_pos) # render image onto surface
     canvas.blit(textSufaceObj, textRectObj)
     pygame.display.update() 
