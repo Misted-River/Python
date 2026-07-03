ba67cf5b6b9c3c4d3c46a6e5e2b835dff9d87480
Misted-River <elli.kourousi@gmail.com>
2026-07-03 11:59:51 +0300

    cleanup code that is unnecessary and placing rocks test

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -3,49 +3,41 @@ import pygame
 pygame.init() # initialise pygame module
 
 position = (0,0)
-clock = pygame.time.Clock()
+#clock = pygame.time.Clock()
 
 
 # set up objects
 player = pygame.image.load('player_placeholder.jpg')# player image -> surface2
 path = pygame.image.load('pathways_place.png')# player image -> surface2
+rocks = pygame.image.load('rocks.png')
 
-image_size = (1000, 1000)
 
 bottom_line_height = 100 # move based on this height, which is centered
 
 
 
 player_rect = player.get_rect() #  original player position (rect(0,0,300,180))
-
 path_pos = path.get_rect()
-width_path = path.get_rect().width
-height_path = path.get_rect().height
+rocks_pos = path_pos
 
-path = pygame.transform.smoothscale(path,(width_path*1.7, height_path*1.7)) # 200,200 is default image size
 
+width_path = path_pos.width
+height_path = path_pos.height
+
+path = pygame.transform.smoothscale(path,(width_path*1.7, height_path*1.7)) 
+rocks = pygame.transform.smoothscale(rocks,(width_path*1.7, height_path*1.7))
 
 #----------------------------------------------------------------------------------
-canvas = pygame.display.set_mode((1920,1080),pygame.RESIZABLE) # canvas size -> creates screen -> background 
-image = pygame.image.load('1.png').convert() # initialise image -> surface2
+canvas = pygame.display.set_mode((1920,1080)) # canvas size -> creates screen -> background 
 
-pygame.draw.line(canvas, 'Black', (0, canvas.get_height() - bottom_line_height), (canvas.get_width(), canvas.get_height() - bottom_line_height), 3) # thickness = 3
-
-
-
-path_pos.midbottom = (canvas.get_width() // 2 + 150, canvas.get_height() - 450 - bottom_line_height - 15)
-player_rect.midbottom = (canvas.get_width() // 2, canvas.get_height() - bottom_line_height - 15)
-
-
-image = pygame.transform.smoothscale(image,(1920,1080)) # sizing image
+image = pygame.image.load('placeholder_bg.png').convert() # initialise image -> surface2
+image = pygame.transform.smoothscale(image,(1920,1080)) # resizing image
 
 pygame.display.set_caption("Welcome to The Pathways") # name of game for window
 
 exit = False
-#refresh = False
 mouse = pygame.mouse.get_pos()
-
-objects = [] # gathering all players etc objects in game
+#objects = [] # gathering all players etc objects in game
 
 
 pygame.event.get()
@@ -63,19 +55,19 @@ y_path = 240
 width = player.get_width()
 height = player.get_height()
 
-width_path = path.get_width()
-height_path = path.get_height()
+#width_path = path.get_width()
+#height_path = path.get_height()
 
 velo = 5 # up down direction
 velo_path = 45 # up down direction
 
 
 
-
-
 canvas.blit(image, dest=position) # render image onto surface, background
 canvas.blit(player, player_rect) # render image onto surface, original position
 canvas.blit(path, path_pos) # render image onto surface, path
+canvas.blit(rocks, path_pos) # render image onto surface, path
+
 pygame.display.update()
 
 while not exit:
@@ -85,7 +77,7 @@ while not exit:
     a = keys[pygame.K_a]
     s = keys[pygame.K_s]
     d = keys[pygame.K_d]
-    print("x:",x_path,"y:",y_path )
+    #print("x:",x_path,"y:",y_path )
 
 
 
@@ -95,7 +87,8 @@ while not exit:
 
     if w and y>0: # key = k_(the key) events 
         y -= velo
-        if (not s) and ( y_path<310) :
+
+        if (not s) and (y_path<310) :
              y_path += velo_path
         else:
             velo = 0
