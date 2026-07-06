91104d65e240de2e3423cab33b27247341257648
Misted-River <elli.kourousi@gmail.com>
2026-07-06 16:43:27 +0300

    movement fixed , masks creation -> learning still

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -2,14 +2,28 @@ import pygame
 
 pygame.init() 
 
+canvas = pygame.display.set_mode((1920,1080)) # canvas size -> creates screen -> background 
+
+
 # constants
 position = (0,0)
 bottom_line_height = 100 # move based on this height, which is centered
 
 # set up objects
-player = pygame.image.load('player_placeholder.jpg')# player image -> surface2
-path = pygame.image.load('pathways_place.png')# patyhway moves -> surface2
-rocks = pygame.image.load('rocks.png') # rocks on pathways, move same as path
+player = pygame.image.load('player_placeholder.jpg').convert_alpha() # player image -> surface2
+path = pygame.image.load('pathways_place.png').convert_alpha() # patyhway moves -> surface2
+rocks = pygame.image.load('rocks.png').convert_alpha() # rocks on pathways, move same as path
+
+# masks
+pa_mask = pygame.mask.from_surface(path)
+path.set_colorkey((0,0,0))
+pl_mask = pygame.mask.from_surface(player)
+player.set_colorkey((0,0,0))
+
+loc_player = (player.get_width(),player.get_height())
+loc_path = (player.get_width(),player.get_height())
+
+
 
 
 # rectangles
@@ -25,7 +39,6 @@ path = pygame.transform.smoothscale(path,(width_path*1.7, height_path*1.7))
 rocks = pygame.transform.smoothscale(rocks,(width_path*1.7, height_path*1.7))
 
 #----------------------------------------------------------------------------------
-canvas = pygame.display.set_mode((1920,1080)) # canvas size -> creates screen -> background 
 
 background = pygame.image.load('placeholder_bg.png').convert() # initialise image -> surface2
 background = pygame.transform.smoothscale(background,(1920,1080)) # resizing image
@@ -36,10 +49,8 @@ exit = False
 mouse = pygame.mouse.get_pos()
 #objects = [] # gathering all players etc objects in game
 
-
 pygame.event.get()
 pygame.display.set_icon(background)
-
 
 # player 
 x = 800 # positioon x
@@ -48,20 +59,22 @@ y = 345 # position y
 x_path = -365
 y_path = 240
 
-
 width = player.get_width()
 height = player.get_height()
 
-velo = 5 # up down direction
-velo_path = 45 # up down direction
+velo = 2 # up down direction
+velo_path = 20 # up down direction
 
 canvas.blit(background, dest=position) # render image onto surface, background
 canvas.blit(player, player_rect) # render image onto surface, original position
 canvas.blit(path, path_pos) # render image onto surface, path
 canvas.blit(rocks, path_pos) # render image onto surface, rocks
+#canvas.blit(pa_mask,path_pos)
 pygame.display.update()
 
 while not exit:
+    pygame.display.update()
+
     keys = pygame.key.get_pressed()
 
     w = keys[pygame.K_w]
@@ -75,42 +88,43 @@ while not exit:
             exit = True
 
     if w and y>0: # key = k_(the key) events 
-        y -= velo
+        y -= velo*2
 
-        if (not s) and (y_path<310) :
+        if w and y>0 and (not s) and (y_path<310) and w and y>0:
             y_path += velo_path
+                
         else:
-            velo = 0
+            velo
 
     if s and  y<1080-height:
-        y += velo
-        if (not w) and y_path>-2525:
+        y += velo*2
+        
+        if s and  y<1080-height and (not w) and y_path>-2525:
             y_path -= velo_path
+                
         else:
-            velo = 0
+            velo
 
     if a and  x>0:
-        x -= velo
+        x -= velo*2
         if (not d) and x_path<-200:
             x_path += velo_path
         else:
-            velo = 10
-            x -= velo*2
-
+            velo
 
     if d and x<1920-width:
-        x += velo
-    
+        x += velo*2
+        
         if (not a) and ( x<1920-width and x_path>-680) :
             x_path -= velo_path
         else:
-            velo = 10
-            x += velo*2
+            velo
 
     canvas.blit(background, dest=position) # render image onto surface, background
     canvas.blit(path, (x_path,y_path)) # render image onto surface, original position
-    canvas.blit(rocks, path_pos) # render image onto surface, rocks
+    canvas.blit(rocks, (x_path,y_path)) # render image onto surface, rocks
     canvas.blit(player, (x,y)) # render image onto surface, original position
+    
     pygame.display.update()
 
 pygame.quit()
