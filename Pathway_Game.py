aea14fcf9184cdb027e3d5206058425e201d0e47
Misted-River <elli.kourousi@gmail.com>
2026-07-13 13:10:15 +0300

    movement when colliding with path border is achieved howvever the code needs refining to remove bugs

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -77,6 +77,8 @@ canvas.blit(player, player_rect) # render image onto surface, original position
 canvas.blit(path, path_rect) # render image onto surface, path
 canvas.blit(rocks, path_rect) # render image onto surface, rocks
 
+hist = []
+
 while not exit:
     keys = pygame.key.get_pressed()
     w = keys[pygame.K_w]
@@ -84,11 +86,14 @@ while not exit:
     s = keys[pygame.K_s]
     d = keys[pygame.K_d]
 
+    
+
     offset = (x - x_path), (y - y_path)
     result = path_mask.overlap(player_mask,(offset))
 
     if result:
-        pass
+        velo = 1 # up down direction
+        velo_path = 0 # up down direction
     else:
         velo = 3 # up down direction
         velo_path = 34 # up down direction
@@ -96,54 +101,61 @@ while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
-            
+
+    if result:
+            if hist[0] == "w":
+                y += velo*4
+            if hist[0] == "s":
+                y -= velo*4
+            if hist[0] == "a":
+                x += velo*4
+            if hist[0] == "d":
+                x -= velo*4
+
     if w and y>0: # key = k_(the key) events 
         y -= velo*2
+        hist.clear()
+        hist.append("w")
         if w and y>0 and (not s) and (y_path<310) and w and y>0:
             y_path += velo_path
-
-        if result and not s:
-            print("w collide not s")
-            y_path -= velo_path
-            y += velo*2
-
-        if result and s:
-            print("w collide s")
-            y_path -= velo_path
-            y += velo*2
-        else:
-            pass
+            hist.clear()
+            hist.append("w")
 
     if s and  y<1080-height:
         y += velo*2
+        hist.clear()
+        hist.append("s")
+
         if s and  y<1080-height and (not w) and y_path>-2525:
             y_path -= velo_path
-            print("s not collide")
 
-        #if result and not w:
-            #print("s collide not w")
-            #y_path += velo_path
-            #y -= velo*2
+            hist.clear()
+            hist.append("s")
 
-        if result and w:
-            print("s collide w")
-            y_path += velo_path
-            y -= velo*2
-
-        else:
-            pass
+  
 
     if a and  x>0:
         x -= velo*2
+        hist.clear()
+        hist.append("a")
+
         if (not d) and x_path<-200:
             x_path += velo_path
+
+            hist.clear()
+            hist.append("a")
         else:
             pass
 
     if d and x<1920-width:
         x += velo*2
+        hist.clear()
+        hist.append("d")
+
         if (not a) and ( x<1920-width and x_path>-680):
             x_path -= velo_path
+            hist.clear()
+            hist.append("d")
         else:
             pass
 
@@ -152,6 +164,7 @@ while not exit:
     canvas.blit(rocks, (x_path,y_path)) # render image onto surface, rocks
     canvas.blit(player, (x,y)) # render image onto surface, original position
     pygame.display.update()
-
+    
 pygame.quit()
+
 
