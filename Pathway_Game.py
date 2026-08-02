05eca0e977653b00fe1e16c28cbd81ca5ecad115
Misted-River <elli.kourousi@gmail.com>
2026-08-02 14:18:47 +0300

    Attempting to make collision detection between mask and mouse pointer when an object is clicked

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 
+
 w = 1920
 h = 1080
 
@@ -16,6 +17,7 @@ bottom_line_height = 100 # move based on this height, which is centered
 path = pygame.image.load('pa_place.png').convert_alpha() # patyhway moves -> surface2
 player = pygame.image.load('play_place.png').convert_alpha() # player image -> surface2
 rocks = pygame.image.load('rocks.png').convert_alpha() # rocks on pathways, move same as path
+dust = pygame.image.load('cosmic_dust.png').convert_alpha() # cosmic dust on pathways, move same as path
 path_rect = path.get_rect()
 
 width_path = path_rect.width
@@ -24,11 +26,14 @@ height_path = path_rect.height
 # resizing to fit
 path = pygame.transform.smoothscale(path,(width_path*1.7, height_path*1.7)) 
 rocks = pygame.transform.smoothscale(rocks,(width_path*1.7, height_path*1.7))
+dust = pygame.transform.smoothscale(dust,(width_path*1.7, height_path*1.7))
 background = pygame.transform.smoothscale(background,(1920,1080)) # resizing image
 
 player.set_colorkey((255,255,255))
+dust.set_colorkey((255,255,255))
 
 path_mask = pygame.mask.from_surface(path)
+dust_mask = pygame.mask.from_surface(dust)
 path_rect = path.get_rect()
 
 player_mask = pygame.mask.from_surface(player)
@@ -36,7 +41,7 @@ player_rect = player.get_rect() #  original player position (rect(0,0,300,180))
 
 # rectangle -> rock
 rocks_rect = path_rect # rocks is same rect as path's recxtangle
-
+dust_rect = path_rect # dust is same rect as path's recxtangle
 # extra for path
 width_path = path_rect.width
 height_path = path_rect.height
@@ -45,7 +50,7 @@ pygame.display.set_caption("Welcome to The Pathways") # name of game for window
 
 exit = False
 # mouse
-#mouse_pos = pygame.mouse.get_pos()
+mx,my = pygame.mouse.get_pos()
 
 pygame.event.get()
 pygame.display.set_icon(background)
@@ -65,26 +70,23 @@ canvas.blit(background, dest=position) # render image onto surface, background
 canvas.blit(player, player_rect) # render image onto surface, original position
 canvas.blit(path, path_rect) # render image onto surface, path
 canvas.blit(rocks, path_rect) # render image onto surface, rocks
+canvas.blit(dust, path_rect) # render image onto surface, dust
+
 
 hist = ["none","none"] # history of last key pressed
 
-def historyadd(lista,key):
-    lista.clear()
-    lista.append(key)
-
 while not exit:
-    keys = pygame.key.get_pressed()
 
+    keys = pygame.key.get_pressed()
     w = keys[pygame.K_w]
     a = keys[pygame.K_a]
     s = keys[pygame.K_s]
     d = keys[pygame.K_d]
-
     shift = keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]
 
     offset = (x - x_path), (y - y_path)
     poi = path_mask.overlap(player_mask,(offset)) 
-    
+
     # variables for movement speed
     if poi: 
         velo = -3
@@ -93,9 +95,22 @@ while not exit:
         velo = 3 # up down direction
         velo_path = 34 # up down direction
 
+    clicks = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: # left click
+                click_position = event.pos
+                clicks = True
+            else:
+                clicks = False
+
+    if clicks:
+        print("click")
+    else:
+        print("no click")
 
     if shift:
         velo = 6 # up down direction
@@ -128,16 +143,13 @@ while not exit:
             y -= velo
             hist[0] ="d"
             hist[1] ="s"
-
     
     if  s and  y<1080-height:
         if not poi and not a and not d:
             y += velo*2
             hist[0] ="s"
-
             if (not w) and (y<1080-height and y_path>-2525):
                 y_path -= velo_path
-
 
         if poi and hist[0] == "w":
             y -= velo
@@ -186,7 +198,10 @@ while not exit:
     canvas.blit(background, dest=position) # render image onto surface, background
     canvas.blit(path, (x_path,y_path)) # render image onto surface, original position
     canvas.blit(rocks, (x_path,y_path)) # render image onto surface, rocks
+    canvas.blit(dust, (x_path,y_path)) # render image onto surface, dust
     canvas.blit(player, (x,y)) # render image onto surface, original position
     pygame.display.update()
     
 pygame.quit()
+
+# more collision detection yayyyyy, but seriously I need to detect if a mouse clicks on some image masks /sarc
