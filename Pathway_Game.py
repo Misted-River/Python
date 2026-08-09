48583818183201eabcaaef74b14310e9c95491b6
Misted-River <elli.kourousi@gmail.com>
2026-08-09 13:04:57 +0300

    first scene's basis is complete! I added a way to move to the next snene now, art for scene 1 is not complete yet however ill probs do that soon or continue on the main game code aka continue onto scene 2 but that will probably be a cutscene i think

--- Pathway_Game.py
+++ Pathway_Game.py
@@ -29,6 +29,7 @@ dust = pygame.image.load('cosmic_dust.png').convert_alpha() # cosmic dust on pat
 dot = pygame.image.load('dot.png').convert_alpha() # cosmic dust on pathways, move same as path
 scrap = pygame.image.load('city_scrap.png').convert_alpha() # cosmic dust on pathways, move same as path
 cracks = pygame.image.load('cracks.png').convert_alpha() # cosmic dust on pathways, move same as path
+line = pygame.image.load("line.png").convert_alpha() # line to go to next scene
 
 dust_label = pygame.image.load('dust_label.png').convert_alpha() # cosmic dust on pathways, move same as path
 ring_label = pygame.image.load('ring_label.png').convert_alpha() # cosmic dust on pathways, move same as path
@@ -46,6 +47,7 @@ rocks = pygame.transform.smoothscale(rocks,(width_path*1.7, height_path*1.7))
 dust = pygame.transform.smoothscale(dust,(width_path*1.7, height_path*1.7))
 scrap = pygame.transform.smoothscale(scrap,(width_path*1.7, height_path*1.7))
 cracks = pygame.transform.smoothscale(cracks,(width_path*1.7, height_path*1.7))
+line = pygame.transform.smoothscale(line,(width_path*1.7, height_path*1.7))
 background = pygame.transform.smoothscale(background,(1920,1080)) # resizing image
 dot = pygame.transform.smoothscale(dot,(15,10)) # resizing image
 
@@ -56,6 +58,7 @@ path_mask = pygame.mask.from_surface(path)
 dust_mask = pygame.mask.from_surface(dust)
 player_mask = pygame.mask.from_surface(player)
 dot_mask = pygame.mask.from_surface(dot) # create mask for circle
+line_mask = pygame.mask.from_surface(line)
 cracks_mask = pygame.mask.from_surface(cracks)
 scrap_mask = pygame.mask.from_surface(scrap)
 
@@ -66,6 +69,7 @@ rocks_rect = path_rect # rocks is same rect as path's recxtangle
 dust_rect = path_rect # dust is same rect as path's recxtangle
 scrap_rect = path_rect # scraps is same rect as path's recxtangle
 cracks_rect = path_rect # cracks is same rect as path's recxtangle
+line_rect = path_rect
 
 # extra for path
 width_path = path_rect.width
@@ -74,6 +78,7 @@ height_path = path_rect.height
 pygame.display.set_caption("Welcome to The Pathways") # name of game for window
 
 exit = False
+scene = 1
 
 pygame.event.get()
 pygame.display.set_icon(background)
@@ -83,6 +88,7 @@ def canvas_blit_end():
     canvas.blit(path, (x_path,y_path)) # render image onto surface, original position
     canvas.blit(rocks, (x_path,y_path)) # render image onto surface, rocks
     canvas.blit(dust, (x_path,y_path)) # render image onto surface, dust
+    canvas.blit(line,(x_path,y_path))
     canvas.blit(player, (x,y)) # render image onto surface, original position
     canvas.blit(scrap, (x_path,y_path)) # render image onto surface, scraps
     canvas.blit(cracks, (x_path,y_path)) # render image onto surface, cracks
@@ -93,6 +99,7 @@ def canvas_blit_start():
     canvas.blit(path, (x_path,y_path)) # render image onto surface, original position
     canvas.blit(rocks, (x_path,y_path)) # render image onto surface, rocks
     canvas.blit(dust, (x_path,y_path)) # render image onto surface, dust
+    canvas.blit(line,(x_path,y_path))
     canvas.blit(player, (x,y)) # render image onto surface, original position
     canvas.blit(scrap, (x_path,y_path)) # render image onto surface, scraps
     canvas.blit(cracks, (x_path,y_path)) # render image onto surface, cracks
@@ -129,142 +136,149 @@ clicks = "none"
 pygame.mixer.music.play(4)
 
 while not exit:
+
+    if scene == 1:
     
+        mx,my = pygame.mouse.get_pos()
 
-    mx,my = pygame.mouse.get_pos()
+        keys = pygame.key.get_pressed()
+        w = keys[pygame.K_w]
+        a = keys[pygame.K_a]
+        s = keys[pygame.K_s]
+        d = keys[pygame.K_d]
+        shift = keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]
 
-    keys = pygame.key.get_pressed()
-    w = keys[pygame.K_w]
-    a = keys[pygame.K_a]
-    s = keys[pygame.K_s]
-    d = keys[pygame.K_d]
-    shift = keys[pygame.K_LSHIFT] or keys[pygame.K_RSHIFT]
+        offset = (x - x_path), (y - y_path)
+        over_off = (x_path - mx ), (y_path - my)
 
-    offset = (x - x_path), (y - y_path)
-    over_off = (x_path - mx ), (y_path - my)
+        poi = path_mask.overlap(player_mask,(offset)) 
+        poi_next = line_mask.overlap(player_mask,(offset)) 
 
-    poi = path_mask.overlap(player_mask,(offset)) 
+        poi_scraps = dot_mask.overlap(scrap_mask,(over_off))
+        poi_cracks = dot_mask.overlap(cracks_mask,(over_off))
+        poi_dot = dot_mask.overlap(dust_mask,(over_off))
 
-    poi_scraps = dot_mask.overlap(scrap_mask,(over_off))
-    poi_cracks = dot_mask.overlap(cracks_mask,(over_off))
-    poi_dot = dot_mask.overlap(dust_mask,(over_off))
+        # variables for movement speed
+        if poi:
+            velo = -6
+            velo_path = -68
+        else:
+            velo = 6 # up down direction
+            velo_path = 68 # up down direction
 
-    # variables for movement speed
-    if poi:
-        velo = -6
-        velo_path = -68
-    else:
-        velo = 6 # up down direction
-        velo_path = 68 # up down direction
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit = True
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # left click
-            click_position = event.pos # location mouse -> middle
-            clicks = True
-        if event.type == pygame.MOUSEBUTTONUP and event.button == 1: # left click
-            click_position = event.pos # location mouse -> middle
-            clicks = False
-        
-
-    if clicks == True:
-        canvas_blit_end()
-        if poi_dot:
-            canvas_blit_end()
-            canvas_blit_labels("dust")
-        elif poi_scraps:
-            canvas_blit_end()
-            canvas_blit_labels("scrap")
-        elif poi_cracks:
-            canvas_blit_end()
-            canvas_blit_labels("ring")
-    else:
-        canvas_blit_end()
-
-
-    if shift:
-        velo = 12 # up down direction
-        velo_path = 136 # up down direction
-
-    if w and y>0: # key = k_(the key) events 
-        if not poi and not a and not d:
-            y -= velo*2
-            hist[0] ="w"
-            if not s and (y>0 and y_path<310)and x_path<-10:
-                y_path += velo_path
-
-        if poi and hist[0] == "s":
-            y += velo
-            hist[0] ="s"
-        if poi and hist[0] == "d":
-            x += velo
-            hist[0] ="d"
-        if poi and hist[0] == "a":
-            x -= velo
-            hist[0] ="a"
-
-        if poi and hist[0] == "a" and hist[1] == "w":
-            x -= velo
-            y -= velo
-            hist[0] ="a"
-            hist[1] ="s"
-        if poi and hist[0] == "d" and hist[1] == "w":
-            x += velo
-            y -= velo
-            hist[0] ="d"
-            hist[1] ="s"
-    
-    if  s and  y<1080-height:
-        if not poi and not a and not d:
-            y += velo*2
-            hist[0] ="s"
-            if (not w) and (y<1080-height and y_path>-2525):
-                y_path -= velo_path
-
-        if poi and hist[0] == "w":
-            y -= velo
-            hist[0] ="w" 
-        if poi and hist[0] == "d":
-            x += velo
-            hist[0] ="d"
-        if poi and hist[0] == "a":
-            x -= velo
-            hist[0] ="a"
-
-    if a and  x>0:
-        if not poi:
-            x -= velo*2
-            hist[0] ="a"
-            if (not d) and x_path<-50:
-                x_path += velo_path
-
-        if poi and hist[0] == "d":
-            x += velo
-            hist[0] ="d"
-        if poi and hist[0] == "w":
-            y -= velo
-            hist[0] ="w"
-        if poi and hist[0] == "s":
-            y += velo
-            hist[0] ="s"
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit = True
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # left click
+                click_position = event.pos # location mouse -> middle
+                clicks = True
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1: # left click
+                click_position = event.pos # location mouse -> middle
+                clicks = False
             
-    if d and x<1920-width:
-        if not poi:
-            x += velo*2
-            hist[0] ="d"
-            if (not a) and (x<1920-width and x_path>-570):
-                x_path -= velo_path
 
-        if poi and hist[0] == "a":
-            x -= velo
-            hist[0] ="a" 
-        if poi and hist[0] == "w":
-            y -= velo
-            hist[0] ="w"
-        if poi and hist[0] == "s":
-            y += velo
-            hist[0] ="s"
+        if clicks == True:
+            canvas_blit_end()
+            if poi_dot:
+                canvas_blit_end()
+                canvas_blit_labels("dust")
+            elif poi_scraps:
+                canvas_blit_end()
+                canvas_blit_labels("scrap")
+            elif poi_cracks:
+                canvas_blit_end()
+                canvas_blit_labels("ring")
+        else:
+            canvas_blit_end()
+
+
+        if shift:
+            velo = 12 # up down direction
+            velo_path = 136 # up down direction
+
+        if w and y>0: # key = k_(the key) events 
+            if not poi and not a and not d:
+                y -= velo*2
+                hist[0] ="w"
+                if not s and (y>0 and y_path<310)and x_path<-10:
+                    y_path += velo_path
+
+            if poi and hist[0] == "s":
+                y += velo
+                hist[0] ="s"
+            if poi and hist[0] == "d":
+                x += velo
+                hist[0] ="d"
+            if poi and hist[0] == "a":
+                x -= velo
+                hist[0] ="a"
+
+            if poi and hist[0] == "a" and hist[1] == "w":
+                x -= velo
+                y -= velo
+                hist[0] ="a"
+                hist[1] ="s"
+            if poi and hist[0] == "d" and hist[1] == "w":
+                x += velo
+                y -= velo
+                hist[0] ="d"
+                hist[1] ="s"
+        
+        if  s and  y<1080-height:
+            if not poi and not a and not d:
+                y += velo*2
+                hist[0] ="s"
+                if (not w) and (y<1080-height and y_path>-2525):
+                    y_path -= velo_path
+
+            if poi and hist[0] == "w":
+                y -= velo
+                hist[0] ="w" 
+            if poi and hist[0] == "d":
+                x += velo
+                hist[0] ="d"
+            if poi and hist[0] == "a":
+                x -= velo
+                hist[0] ="a"
+
+        if a and  x>0:
+            if not poi:
+                x -= velo*2
+                hist[0] ="a"
+                if (not d) and x_path<-50:
+                    x_path += velo_path
+
+            if poi and hist[0] == "d":
+                x += velo
+                hist[0] ="d"
+            if poi and hist[0] == "w":
+                y -= velo
+                hist[0] ="w"
+            if poi and hist[0] == "s":
+                y += velo
+                hist[0] ="s"
+                
+        if d and x<1920-width:
+            if not poi:
+                x += velo*2
+                hist[0] ="d"
+                if (not a) and (x<1920-width and x_path>-570):
+                    x_path -= velo_path
+
+            if poi and hist[0] == "a":
+                x -= velo
+                hist[0] ="a" 
+            if poi and hist[0] == "w":
+                y -= velo
+                hist[0] ="w"
+            if poi and hist[0] == "s":
+                y += velo
+                hist[0] ="s"
+        if poi_next:
+            canvas.blit(background, dest=position) # render image onto surface, background
+            scene = 2
+    if scene == 2:
+        canvas.blit(background, dest=position) # render image onto surface, background
 
     pygame.display.update()
 
